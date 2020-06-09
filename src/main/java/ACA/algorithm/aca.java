139bfec3947b5f7f7fcf4b63a96967348403686f
ellenbboe <1194261806@qq.com>
2020-06-09 20:10:18 +0800

    :fire:

--- src/main/java/ACA/algorithm/aca.java
+++ src/main/java/ACA/algorithm/aca.java
@@ -1,4 +1,28 @@
 package ACA.algorithm;
 
+import ACA.entity.Location;
+
+import java.util.LinkedList;
+
 public class aca {
+    int[][] maps = new int[10][10];
+    LinkedList<Location> results = new LinkedList<>();
+    int sum = 9;
+    int count = 1;
+
+
+
+
+    public Location step(int x,int y)
+    {
+        if(maps[x][y] == 1)
+        {
+            count++;
+            x=x+1;
+        }else{
+            y=y+1;
+        }
+        step(x,y);
+        return new Location(x,y);
+    }
 }
