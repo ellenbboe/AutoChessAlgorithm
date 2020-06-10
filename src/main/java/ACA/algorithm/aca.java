15d626d763c1360b2b1ec137601e2a018081782c
ellenbboe <1194261806@qq.com>
2020-06-10 18:38:13 +0800

    :fire:

--- src/main/java/ACA/algorithm/aca.java
+++ src/main/java/ACA/algorithm/aca.java
@@ -1,12 +1,13 @@
 package ACA.algorithm;
-
 import ACA.entity.Location;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.Collection;
 import java.util.LinkedList;
 
+@Slf4j
 public class aca {
     int[][] maps = new int[10][10];
-    LinkedList<Location> results = new LinkedList<>();
     int sum = 9;
     int count = 1;
 
@@ -15,6 +16,10 @@ public class aca {
         if(maps[x][y] == 1)
         {
             count++;
+            if(count == sum)
+            {
+                return new Location(x,y);
+            }
             x=x+1;
         }else{
             y=y+1;
@@ -27,7 +32,19 @@ public class aca {
     {
         for(int i = 0;i<maps.length;i++)
         {
-            step(i,0);
+            LinkedList<Location> results = new LinkedList<>();
+            results.add(step(i,0));
+            for(Location a :results)
+            {
+               log.info(a.toString());
+            }
         }
+    }
+
+    public static void main(String[] args) {
+        aca a = new aca();
+        a.maps[9][9]= 1;
+        a.maps[5][9]= 1;
+        a.go();
     }
 }
--- src/main/java/ACA/entity/Location.java
+++ src/main/java/ACA/entity/Location.java
@@ -24,4 +24,13 @@ public class Location {
         this.x = x;
         this.y = y;
     }
+
+
+    @Override
+    public String toString() {
+        return "(" +
+                "x=" + x +
+                ", y=" + y +
+                ')';
+    }
 }
